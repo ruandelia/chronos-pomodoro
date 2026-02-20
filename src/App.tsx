aba4985aaddbf23ce8c589d792d3f0fc86a7c4ec
ruan <ruanvictordelia11@gmali.com>
2026-02-20 16:55:13 -0300

    Criando a estrutura HTML e CSS do nosso app para cada seção do site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <div className='container-fluid'>
+        <div className='container'>
+          <div className='content'>
+            <section>LOGO</section>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
