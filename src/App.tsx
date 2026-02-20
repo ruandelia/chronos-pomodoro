7fcd3d97c585770564841160caa4ab518d750a59
ruan <ruanvictordelia11@gmali.com>
2026-02-20 12:57:59 -0300

    Importando e usando arquivos globais CSS.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 // App
 // HeaderHeading
 // Todas as primeiras letras em maiúsculas
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
-    console.log('Olá');
+  console.log('Olá');
   return (
     <>
       <h1>Olá mundo! (do App) </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>  
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </>
-    );
+  );
 }
