d3ca31309cf3cfaaf78b10ca3aa28586b043db7f
ruan <ruanvictordelia11@gmali.com>
2026-02-20 17:54:51 -0300

    Criei um componente para a estrutura html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <div className='container-fluid'>
-        <div className='container'>
-          <div className='content'>
-            <section>LOGO</section>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Heading>Testando meu componente Heading.</Heading>
+      </Container>
     </>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+import type React from "react"
+import styles from "./Container.module.css";
+
+type ContainerProps = {
+    children: React.ReactNode
+}
+
+export function Container({ children }: ContainerProps){
+    return (
+        <div className={styles.container}>
+          <div className={styles.content}>
+            {children}
+          </div>
+        </div>
+    );
+}
