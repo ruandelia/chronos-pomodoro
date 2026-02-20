4808066f5758027ef4d6b6eb3dc3f266a0c4bbe5
ruan <ruanvictordelia11@gmali.com>
2026-02-20 18:39:27 -0300

    Criando o componente de logo com ícone e link.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
 
 export function App() {
    return( <>
       <Container>
-        <Heading>Testando meu componente Heading.</Heading>
+        <Logo/>
       </Container>
     </>
   );
