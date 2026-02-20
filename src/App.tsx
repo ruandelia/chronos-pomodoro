ba2ef69dffc14d495eb640110e2178ae0d245c2d
ruan <ruanvictordelia11@gmali.com>
2026-02-20 16:01:01 -0300

    Variáveis e Reset de CSS do Chronos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-// PascalCase
-// App
-// HeaderHeading
-// Todas as primeiras letras em maiúsculas
 import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Olá');
   return (
     <>
-      <Heading attr={123} attr2='String'>Olá Ruan</Heading>
+      <Heading attr={123} attr2='String'>Olá Mundo!</Heading>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </>
   );
