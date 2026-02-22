c09e2d9a917c13c01317132ec3d45a053346c3b3
ruan <ruanvictordelia11@gmali.com>
2026-02-22 01:35:39 -0300

    Criando formulário da página inicial.

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,42 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 
 export function App() {
-   return( <>
+  return (
+    <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
 
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
 
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input id='input' type='text' />
+          </div>
+       
+
+        <div className='formRow'>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className='formRow'>
+          <p>Ciclos</p>
+          <p>0 0 0 0</p>
+        </div>
+
+        <div className='formRow'>
+          <button>Enviar</button>
+        </div>
+         </form>
       </Container>
     </>
   );
 }
-    
